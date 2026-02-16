b92ac34386ae6fd6fbd560e531c779f66f398fad
mw224cg <138625041+mw224cg@users.noreply.github.com>
2026-02-16 12:10:17 +0100

    Update Main.java

--- Labb4/Main.java
+++ Labb4/Main.java
@@ -28,6 +28,9 @@ public class Main {
 
     //Måste göra en main. init trådar + join etc etc
     //Måste fixa felhantering i prog, try/catch block etc
+    //möjligt förslag för helt fair program:
+    // ReentrantLock + Condition
     
 }
+
 
