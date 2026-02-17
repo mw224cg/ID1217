faf9d9542d9c2d11ce2a4eecfdf906d33549c21e
mw224cg <138625041+mw224cg@users.noreply.github.com>
2026-02-17 23:09:23 +0100

    Modify sleep duration in Vehicle class
    Updated sleep duration to 5000ms for travel and repair simulation.

--- Labb4/Vehicle.java
+++ Labb4/Vehicle.java
@@ -1,3 +1,11 @@
+/**
+ * Varje fordon är en tråd som representerar ett fordon som behöver repareras.
+ * Varje fordon har en typ (A, B, C) och ett antal gånger det ska repareras.
+ * Varje fordon försöker repareras i stationen, väntar om det inte finns plats, och signalerar när det är klart.
+ * Varje fordon simulerar restid och reparationstid med Thread.sleep().
+ * 
+ */
+
 public class Vehicle extends Thread{
     private final RepairStation station;
     private final VehicleType type;
@@ -12,11 +20,11 @@ public class Vehicle extends Thread{
     public void run(){
         try {
             for(int i = 0; i < numRepairs; i++){
-                Thread.sleep(1000); //simulera restid till stationen
+                Thread.sleep(5000); //simulera restid till stationen
 
                 station.requestRepair(type);
 
-                Thread.sleep(1000); //simulera reparationstid
+                Thread.sleep(5000); //simulera reparationstid
 
                 station.releaseRepair(type);
             }
