9d062a0c9ecb549ab2d4d96f7dfae9b0266a9fae
mw224cg <138625041+mw224cg@users.noreply.github.com>
2026-02-17 23:08:15 +0100

    Update Main.java

--- Labb4/Main.java
+++ Labb4/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /**
  * En reparatör kan repararera fordon av typen A,B och C
  * Reparatören kan samtidigt reparera som mest:
@@ -22,15 +25,47 @@
  * Fordonet anropar releaseRepair():
  * => Antal platser i repairStation uppdateras
  * => NotifyAll() väcker alla väntande trådar, dessa kontrollerar i sin tur om det finns plats för dem att repareras.
+ * 
+ * Programmet kör simuleringen n antal gånger för varje fordon (numRepairs i Vehicle).
  */
 
 public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        int c = 2;
+        int v = 3;
+        int threads = 6;
+        int numRepairs = 2;
 
-    //Måste göra en main. init trådar + join etc etc
-    //Måste fixa felhantering i prog, try/catch block etc
-    //möjligt förslag för helt fair program:
-    // ReentrantLock + Condition
+        RepairStation repairStation = new RepairStation(a, b , c, v);
+        ArrayList<Vehicle> vehicles = new ArrayList<>();
+
+        for(int i = 0; i < threads/3; i++){
+            vehicles.add(new Vehicle(repairStation, VehicleType.A, numRepairs));
+            vehicles.add(new Vehicle(repairStation, VehicleType.B, numRepairs));
+            vehicles.add(new Vehicle(repairStation, VehicleType.C, numRepairs));
+        }
+
+        System.out.printf("Threads: %d | a = %d b = %d, c = %d, v = %d, Number of repairs/vehicle: %d\n",
+        threads, a, b, c, v, numRepairs);
+        System.out.println("Starting threads\n");
+        System.out.println("EVENT : TYPE|    THREAD    | OCCUPIED COUNT\n");
+
+
+        for(Vehicle vehicle : vehicles){
+            vehicle.start();
+        }
+
+        for(Vehicle vehicle : vehicles){
+            try {
+                vehicle.join();
+            } catch (Exception e) {
+            }
+        }
+
+        System.out.println("Program finished\n");
+    }
+
     
 }
-
-
